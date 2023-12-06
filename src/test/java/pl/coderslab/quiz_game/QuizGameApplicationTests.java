b12308ffe208071b23a25ad8b4eb120985d1ccac
Nicpon99 <Nicpon044@onet.pl>
2023-12-06 20:06:27 +0100

    first commit

---
+++ src/test/java/pl/coderslab/quiz_game/QuizGameApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.quiz_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
